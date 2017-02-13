bee668dced2fb4f0ee83084e897b6d33caa9c30b
ConstantJoe <joseph.finnegan@tutanota.com>
2017-02-13 19:28:09 +0000

    Filling in some methods

--- platform/avr-atmega128rfa1/rn2483.c
+++ platform/avr-atmega128rfa1/rn2483.c
@@ -1,13 +1,39 @@
 
 
-void sys_sleep(int length)
+int sys_sleep(unsigned long length)
 {
-	rs232_print(RS232_PORT_1, "tester");
+
+	if(length > 100)
+	{
+		char* command = "sys sleep ";
+
+		//TODO: generate len_str
+		//see http://stackoverflow.com/questions/2709713/how-to-convert-unsigned-long-to-string
+
+		char *result = malloc(strlen(command)+strlen(len_str)+1);
+		strcpy(result, command);
+		strcat(result, len_str);
+		rs232_print(RS232_PORT_1, result);
+
+		PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message_1)
+
+
+		//TODO: if "ok", return 1. Else return 0.
+	}
+	else
+	{
+
+		//TODO: Print err message, and return 0.
+	}
 }
 
-void sys_reset()
+char* sys_reset()
 {
 	rs232_print(RS232_PORT_1, "sys reset\r\n");
+
+	PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message_1)
+
+	return (char *)data; //use of ev and data not as simple as this. But this is basically the idea.
 }
 
 void sys_eraseFW()
@@ -18,120 +44,326 @@ void sys_eraseFW()
 void sys_factoryRESET()
 {
 	rs232_print(RS232_PORT_1, "sys factoryRESET\r\n");
+
+	PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message_1)
+
+	return (char *)data; //use of ev and data not as simple as this. But this is basically the idea.
 }
+
+//System Set Commands
 
 /*Modify EEPROM at address to data. Both address and data must be inputted as hex values
 	Can modify addresses from 300 to 3FF.
 */
-void sys_set_nvm(int address, int data)
+int sys_set_nvm(unsigned int address, unsigned char data)
 {
+	if(address >= 0x300 && address <= 0x3FF)
+	{
+		if(data >= 0x00 && <= 0xFF)
+		{
+			char* command = "sys set nvm ";
 
+			//TODO: address to char* (hex version)
+
+			//TODO: data to char* (hex version)
+
+			PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message_1)
+
+			//if "ok", return 1.
+			//else return 0
+		}
+		else
+		{
+			//print err message, return 0.
+		}
+	}
+	else
+	{
+		//print err message, return 0.
+	}
 }
 
 /*Modify unused pins on the module
-Pinname can be GPIO0-GPIO4, UART_CTS, UART_RTS, TEST0, TEST1
+Pinname can be GPIO0-GPIO14, UART_CTS, UART_RTS, TEST0, TEST1
 Pinstate can be 0 or 1.
 */
-void sys_set_pindig(char* pinname, int pinstate)
+int sys_set_pindig(char* pinname, int pinstate)
 {
+	if(pinname == "GPIO0" | pinname == "GPIO1" | pinname == "GPIO2" | pinname == "GPIO3" | pinname == "GPIO4" 
+		| pinname == "GPIO5" | pinname == "GPIO6" | pinname == "GPIO7" | pinname == "GPIO8" | pinname == "GPIO9"
+		| pinname == "GPIO10" | pinname == "GPIO11" | pinname == "GPIO12" | pinname == "GPIO13" | pinname == "GPIO14"
+		| pinname == "UART_CTS" | pinname == "UART_RTS" | pinname == "TEST0" | pinname == "TEST1")
+	{
+		if(pinstate == 0 | pinstate == 1)
+		{
+			char* command = "sys set pindig ";
 
+			//TODO: pinstate to char*
+
+			PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message_1);
+
+			//if "ok", return 1.
+			//else return 0
+		}
+		else
+		{
+			//print err message, return 0.
+		}
+	}
+	else
+	{
+		//print err message, return 0.
+	}
 }
 
-void sys_get_ver()
+//System Get Commands
+
+char* sys_get_ver()
 {
 	rs232_print(RS232_PORT_1, "sys get ver\r\n");
+
+	PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message_1)
+
+	return (char *)data; 
 }
 
-void sys_get_nvm(int address)
+unsigned char sys_get_nvm(int address)
 {
+	if(address >= 0x300 && address <= 0x3FF)
+	{
+		char* command = "sys get nvm ";
 
+		//TODO: address to char*
+
+		PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message_1);
+
+		//TODO: data to unsigned int.
+
+	}
+	else
+	{
+		//print err message, return 0.
+	}
 }
 
-void sys_get_vdd()
+unsigned int sys_get_vdd()
 {
 	rs232_print(RS232_PORT_1, "sys get vdd\r\n");
+
+	PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message_1);
+
+	//TODO: data to unsigned int
 }
 
 void sys_get_hweui()
 {
 	rs232_print(RS232_PORT_1, "sys get hweui\r\n");
+
+	PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message_1);
+
+	return (char *)data; 
 }
+
+//MAC Commands
 
 /*band can be 868 or 433*/
-void mac_reset(int band)
+int mac_reset(int band)
 {
+	if(band == 433 | band == 868)
+	{
+		char* command = "mac reset ";
+		
+		//TODO: int to char*
 
+		PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message_1);
+
+		//TODO: if "ok", return 1. Else return 0.
+	}
+	else
+	{
+		//print err message, return 0.
+	}
 }
 
-void mac_tx(char* type, int portno, char* data)
+void mac_tx(char* type, unsigned int portno, char* data)
 {
+	if(type == "cnf" | "uncnf")
+	{
+		if(portno >= 1 && <= 223)
+		{
+			char* command == "mac tx ";
 
+			//TODO: unsigned int to char
+
+			PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message_1);
+
+			//TODO: response is complex; there's two parts
+		}
+		else
+		{
+			//print err message, return "0"
+		}
+	}
+	else
+	{
+		//print err message, return "0"
+	}
 }
 
 void mac_join(char* mode)
 {
+	if(mode == "otaa" | mode == "abp")
+	{
+		char* command == "mac join ";
 
+		//TODO: concat strings
+
+		PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message_1);
+
+		//TODO: response is complex
+	}
+	else
+	{
+		//print err message, return "0"
+	}
 }
 
-void mac_save()
+int mac_save()
 {
 	rs232_print(RS232_PORT_1, "mac save\r\n");
+
+	PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message_1);
+
+	// if data is ok, return 1. Else return 0.
 }
 
 void mac_forceENABLE()
 {
 	rs232_print(RS232_PORT_1, "mac forceENABLE\r\n");
+
+	PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message_1);
+
+	// if data is ok, return 1. Else return 0.
 }
 
-void mac_pause()
+unsigned int mac_pause()
 {
 	rs232_print(RS232_PORT_1, "mac pause\r\n");
+
+	PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message_1);
+
+	// convert data to unsigned int, return it.
 }
 
-void mac_resume()
+int mac_resume()
 {
 	rs232_print(RS232_PORT_1, "mac resume\r\n");
+
+	PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message_1);
+
+	// if data is ok, return 1. Else return 0.
 }
 
+//MAC Set Commands
 
-void mac_set_devaddr(int address)
+int mac_set_devaddr(unsigned int address)
 {
+	char* command == "mac set devaddr ";
+	//TODO: convert unsigned int to char*
 
+	PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message_1);
+
+	// if data is ok, return 1. Else return 0.
 }
 
-void mac_set_deveui(char* deveui)
+int mac_set_deveui(char* deveui)
 {
+	//TODO: check if string is in hex format, correct length
 
+	char* command == "mac set deveui ";
+
+	//TODO: concat char*s, send off
+
+	PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message_1);
+
+	// if data is ok, return 1. Else return 0.
 }
 
-void mac_set_appeui(char* appeui)
+int mac_set_appeui(char* appeui)
 {
+	//TODO: check if string is in hex format, correct length
 
+	char* command == "mac set appeui ";
+
+	//TODO: concat char*s, send off
+
+	PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message_1);
+
+	// if data is ok, return 1. Else return 0.
 }
 
-void mac_set_nwkskey(char* nwksesskey)
+int mac_set_nwkskey(char* nwksesskey)
 {
+	//TODO: check if string is in hex format, correct length
 
+	char* command == "mac set nwkskey ";
+
+	//TODO: concat char*s, send off
+
+	PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message_1);
+
+	// if data is ok, return 1. Else return 0.
 }
 
-void mac_set_appskey(char* appSesskey)
+int mac_set_appskey(char* appSesskey)
 {
+	//TODO: check if string is in hex format, correct length
 
+	char* command == "mac set appskey ";
+
+	//TODO: concat char*s, send off
+
+	PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message_1);
+
+	// if data is ok, return 1. Else return 0.
 }
 
-void mac_set_appkey(char* appkey)
+int mac_set_appkey(char* appkey)
 {
+	//TODO: check if string is in hex format, correct length
 
+	char* command == "mac set appkey ";
+
+	//TODO: concat char*s, send off
+
+	PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message_1);
+
+	// if data is ok, return 1. Else return 0.
 }
 
 void mac_set_pwridx(int pwrIndex)
 {
+	//TODO: check if string is in hex format, correct length
 
+	if(pwrIndex >=0 && pwrIndex <= 5)
+	{
+		char* command == "mac set pwridx ";
+
+		//TODO: convert int to char*
+
+		PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message_1);
+
+		// if data is ok, return 1. Else return 0.
+	}
+	else
+	{
+
+	}
 }
 
 void mac_set_dr(int dataRate)
 {
-
+	
 }
 
 void mac_set_adr(char* state)
